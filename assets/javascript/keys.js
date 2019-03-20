e4be7c717028848f486eba00c2f60fa052e49d37
ShoreFooted <ricbroker@gmail.com>
2019-03-20 06:20:28 -0700

    pushing hotel-food-drink module files to master

---
+++ assets/javascript/keys.js
@@ -0,0 +1,8 @@
+var foursquare = {
+    clientId: "AUPQ31UHB23UXJGBO2QH4KIHI1FYOXABIRM2TVSCJPTAJNUI",
+    clientSecret: "BB5OOVS5B24BE1N224BDGKQQVGTUQQCD1YKZKQNKCEH12YOD"
+}
+
+var bands = {
+    id: "codingbootcamp"
+}
